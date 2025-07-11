cc2f5960f23e595ebfd04ee2044ff6dae26129cd
Sarthak <sarthak.00puri@gmail.com>
2025-07-12 04:11:27 +0530

    FEAT: added preprocessed test data according to the training data

---
+++ preprocessing.py
@@ -0,0 +1,51 @@
+
+import pandas as pd
+
+#Constants
+product_map = {
+    'VBurger': 'Veg Burger',
+    'Veggie Burger': 'Veg Burger',
+    'V. Burger': 'Veg Burger',
+    'Veg Burger': 'Veg Burger',
+    
+    'Fries': 'Fries',    
+
+    'F Burger': 'Falafel Burger',
+    'Falafel Burger': 'Falafel Burger',
+    
+    'Chz Burger': 'Cheese Burger',
+    'Cheese Burger': 'Cheese Burger',
+
+    'Chicken Burger': 'Chicken Burger',
+    'Chk Burger': 'Chicken Burger',
+    'Chicken Brg': 'Chicken Burger',
+    'Chicken Br': 'Chicken Burger',
+
+    'Coca-Cola': 'Coca-Cola',
+    'Coke': 'Coca-Cola',
+
+    # Instructions — 'Custom Order'
+    'X-CHeese': 'Custom',
+    'NO TOMATOES': 'Custom',
+    'No Tom Plz': 'Custom',
+    'No Chz': 'Custom',
+    'Absolute no cheese': 'Custom',
+    'No Lettuce': 'Custom',
+    'Fast Please': 'Custom',
+    'Extra Cheese': 'Custom',
+    'Carmalized Onions': 'Custom',
+}
+
+special_map={
+    'Yes':1,
+    'No':0,
+    'Y':1,
+    'N':0,
+    'nO':0
+}
+
+upper_limit_s3=34050.15
+
+# def clean_data(df):
+#     df.drop(['Product_Calorie','mods','Promo_applied','weather'], axis=1, inplace=True)
+    
